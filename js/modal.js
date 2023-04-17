0f96f4e33fa588fbbf7b3f79b30c62c87eb5d408
Miki <ghermaico135@gmail.com>
2023-04-17 17:05:37 +0300

    create modal.js and modify index.html for popup

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+// Creating pop up modal
+// creating js object
+
+const worksArray = [
+  {
+      featImage: [
+          './images/photos/portfolio-pic-1.png',
+          './images/photos/portfolio-pic-2.png',
+          './images/photos/portfolio-pic-3.png',
+          './images/photos/portfolio-pic-4.png'
+      ],
+      title: ['Tonic', 'Multi-Post Stories'],
+      type: ['CANOPY', 'Backend', '2015'],
+      desc: [
+          'A daily selection of privately personalized reads; no accounts or sign-ups required.'
+      ],
+      techs: ['html', 'css', 'javascript'],
+      details: {btn1: 'See Project', btn2: 'See Project', btn3: 'See Project', btn4: 'See Project'},
+      liveLink: "https://github.io/ometman/my-portfolio",
+      sourceLink: "https://github.io/ometman/my-portfolio.git"
+  }
+]
