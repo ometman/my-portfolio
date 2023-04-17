dd46a52d0219029e8160f1af9ede2a1481ae3fe0
ometman <revomet@gmail.com>
2023-04-17 21:13:17 +0000

    Modify styles, index and main js files for mobile menu look and response

--- js/main.js
+++ js/main.js
@@ -2,16 +2,18 @@
 //get elements
 const mobMenu = document.querySelector('#mobile-menu');
 const mobMenuOpen = document.querySelector('#mm-wrapper');
-const mobMenuClose = document.querySelector('#mm-menu-close');
+const mobMenuClose = document.querySelectorAll('.m-menu-closebtn, .m-menu-close');
 
 // event to open menu
 mobMenuOpen.addEventListener('click', (e) => {
     e.preventDefault();
-    mobMenu.style.display = 'flex';     
+    mobMenu.style.display = 'flex';
+    mobMenu.style.filter = 'blur-bg';
 })
 
-// event to close menu
-mobMenuClose.addEventListener('click', (e) => {
+// event to close menu when icon or menu item is clicked
+mobMenuClose.forEach(link => link.addEventListener('click', (e) => {
     e.preventDefault();
-    mobMenu.style.display = 'none';     
-})
+    mobMenu.style.display = 'none';
+
+}));
