64552525dedb65339bd105c521314bcd61e326cb
ometman <revomet@gmail.com>
2023-04-18 14:53:45 +0000

    Modify main.js, styles.css and index.html for mobile menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 // mobile menu
 //get elements
-const mobMenu = document.querySelector('#mobile-menu');
-const mobMenuOpen = document.querySelector('#mm-wrapper');
+const headline = document.querySelector('.headline-section');
+const mobMenuOpen = document.querySelector('#mm-icon-wrapper');
+const mobMenuNav = document.querySelector('.m-menu-nav');
 const mobMenuClose = document.querySelectorAll('.m-menu-closebtn, .m-menu-close');
+const blurEl = document.querySelectorAll('.logo, .mm-icon-wrapper, .headline-section, .mob-works-section, .about-me-section, .footer');
 
 // event to open menu
 mobMenuOpen.addEventListener('click', (e) => {
-    e.preventDefault();
-    mobMenu.style.display = 'flex';
-    mobMenu.style.filter = 'blur-bg';
+  e.preventDefault();
+  mobMenuNav.style.display = 'flex';
+  mobMenuNav.classList.add('m-menu-color-blend');
+  blurEl.forEach(el => el.classList.add('blur-bg'));
 })
 
 // event to close menu when icon or menu item is clicked
 mobMenuClose.forEach(link => link.addEventListener('click', (e) => {
-    e.preventDefault();
-    mobMenu.style.display = 'none';
-
+  mobMenuNav.style.display = 'none';
+  blurEl.forEach(el => el.classList.remove('blur-bg'));
+  e.preventDefault();
 }));
+
+mobMenuClose.forEach(link => link.addEventListener('click', (e) => {
+    mobMenuNav.style.display = 'none';
+    blurEl.forEach(el => el.classList.remove('blur-bg'));
+    e.preventDefault();
+  }));
