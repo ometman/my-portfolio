d83e587236cc11d25a8081ce0ff15fe8fb3cc5c7
ometman <revomet@gmail.com>
2023-04-20 18:07:13 +0000

    Fix conflict, linters and mobile menu errors

--- js/main.js
+++ js/main.js
@@ -1,38 +1,19 @@
 // mobile menu
 // get elements
-const mobMenu = document.querySelector('#mobile-menu');
-const mobMenuOpen = document.querySelector('#mm-wrapper');
-const mobMenuClose = document.querySelector('#mm-menu-close');
 const mobMenuOpen = document.querySelector('#mm-icon-wrapper');
 const mobMenuNav = document.querySelector('.m-menu-nav');
 const mobMenuClose = document.querySelectorAll('.m-menu-closebtn, .m-menu-close');
 const blurEl = document.querySelectorAll('.logo, .mm-icon-wrapper, .headline-section, .mob-works-section, .about-me-section, .footer');
 
-
 // event to open menu
 mobMenuOpen.addEventListener('click', (e) => {
   e.preventDefault();
-  mobMenu.style.display = 'flex';
-});
-
-// event to close menu
-mobMenuClose.addEventListener('click', (e) => {
-  e.preventDefault();
-  mobMenu.style.display = 'none';
-});
-
   mobMenuNav.style.display = 'flex';
   mobMenuNav.classList.add('m-menu-color-blend');
   blurEl.forEach((el) => el.classList.add('blur-bg'));
 });
 
 // eventS to close menu when icon or menu item when clicked
-mobMenuClose.forEach((link) => link.addEventListener('click', () => {
-  mobMenuNav.style.display = 'none';
-  blurEl.forEach((el) => el.classList.remove('blur-bg'));
-}));
-
-//
 mobMenuClose.forEach((link) => link.addEventListener('click', () => {
   mobMenuNav.style.display = 'none';
   blurEl.forEach((el) => el.classList.remove('blur-bg'));
