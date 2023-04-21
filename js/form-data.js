14ea5caf2a15e91ec894a61795e6c032de730676
ometman <revomet@gmail.com>
2023-04-21 05:57:18 +0000

    Fix linters errors

--- js/form-data.js
+++ js/form-data.js
@@ -1,26 +1,20 @@
 // Form email validation and storing user data
-//get elements
+// get elements
 const form = document.querySelector('#contact-form');
-const username = document.querySelector('#username') 
 const email = document.querySelector('#email');
-const message = document.querySelector('#message');
 const emailErrorId = document.querySelector('#email-error');
 
-// email validation function 
-function validateEmail(emailStr) { return emailStr.toLowerCase() !== emailStr }
+// email validation function
+function validateEmail(emailStr) { return emailStr.toLowerCase() !== emailStr; }
 
-// add event to display error 
+// add event to display error
 form.addEventListener(('submit'), (e) => {
+  // getting the values from elements
+  const userEmail = email.value;
 
-    // getting the values from elements
-    const userEmail = email.value;
-
-    // validating email
-    if(validateEmail(userEmail)) {  
-        e.preventDefault(); 
-        emailErrorId.style.visibility = 'visible' 
-    }
+  // validating email
+  if (validateEmail(userEmail)) {
+    e.preventDefault();
+    emailErrorId.style.visibility = 'visible';
+  }
 });
-
-
-
